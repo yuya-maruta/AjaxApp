da0014a48e525839d31064c04c801248026470a8
yuya-maruta <ymx08110112@gmail.com>
2020-08-05 19:47:46 +0900

    Revert "既読機能の実装"
    This reverts commit 36856b3ef89d5ce83617c29aac6974c9efc78537.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,18 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-  end
+  
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
   end
 
   def checked
+    
+
+
     post = Post.find(params[:id])
     if post.checked then
       post.update(checked: false)
@@ -20,6 +22,5 @@ class PostsController < ApplicationController
 
     item = Post.find(params[:id])
     render json: { post: item }
-    
   end
 end
